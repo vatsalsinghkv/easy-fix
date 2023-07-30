e855692590f2c8e2f03234ca3a17a9ab3f97e2b8
vatsal <vatsalsinghkv@gmail.com>
2023-07-30 12:21:29 +0530

    feat: design system add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
+        mono: ['Roboto Mono', 'monospace'],
+      },
+      colors: {
+        bg: 'var(--color-bg)',
+        'bg-secondary': 'var(--color-bg-secondary)',
+        accent: 'var(--color-accent)',
+        'accent-light': 'var(--color-accent-light)',
+        text: 'var(--color-text)',
+        'dark-1': 'var(--color-dark-1)',
+        'dark-2': 'var(--color-dark-2)',
+        'dark-3': 'var(--color-dark-3)',
       },
     },
   },
