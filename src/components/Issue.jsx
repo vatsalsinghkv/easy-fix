0107b317fd07f45c6b4cc2f5f3cc24099df31e1e
vatsal <vatsalsinghkv@gmail.com>
2023-07-30 22:01:41 +0530

    fix: issue height

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -3,7 +3,7 @@ import Label from './Label';
 const Issue = ({ title, url, labels }) => {
   return (
     <a
-      className='block p-5 border rounded-md border-dark-3 hover:bg-bg-secondary focus:bg-bg-secondary group'
+      className='block p-5 border rounded-md border-dark-3 hover:bg-bg-secondary min-h-[105px] focus:bg-bg-secondary group'
       href={url}
       target='_blank'
       rel='noreferrer'
