f1624186e73d430b086cf50806a082d0f6ffbc8c
vatsal <vatsalsinghkv@gmail.com>
2023-07-30 23:25:46 +0530

    feat: loader add

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Issue from './Issue';
 import Label from './Label';
+import Loader from './Loader';
 import Logo from './Logo';
 
-export { Issue, Label, Logo };
+export { Issue, Label, Logo, Loader };
