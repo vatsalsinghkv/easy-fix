58fc1d2e529ce176b54093944ce82da1025ccb54
akartis-dev <sramamonjinirina@gmail.com>
2023-09-30 20:07:07 +0300

    feat install typescript and migrate

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.jsx';
+import App from './App';
 import UseUrlProvider from './hooks/use-url.jsx';
 import './index.css';
 
