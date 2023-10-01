70f9d54e02be3aba3dacc6b3662b48e65cfb26d9
Jose <cardenas.josediego@gmail.com>
2023-10-01 00:55:57 -0500

    test: unit testing with vitest

---
+++ src/__tests__/components/Loader.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { Loader } from '../../components';
+
+describe('Loader', () => {
+  it('renders the loader', () => {
+    render(<Loader />);
+
+    const loader = screen.getByRole('status');
+    expect(loader).toBeInTheDocument();
+  });
+
+  it('has the correct classes', () => {
+    render(<Loader />);
+
+    const loader = screen.getByRole('status');
+    expect(loader).toHaveClass(
+      'inline-block h-14 w-14 animate-spin text-accent rounded-full border-4 border-solid border-current border-r-dark-3 align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+    );
+  });
+});
