388d90a449829d74d14c39172a13d76cf0bf81f1
S4JN <srajanacharya09@gmail.com>
2023-10-01 14:49:04 +0530

    feat:sort-issues

--- src/hooks/use-url.jsx
+++ src/hooks/use-url.jsx
@@ -1,12 +1,14 @@
+import { DEFAULT_LANGUAGE, DEFAULT_SORT_BY } from '../utils/config';
 import { createContext, useContext, useEffect, useState } from 'react';
 
 import { request } from '../api/request';
-import { DEFAULT_LANGUAGE, DEFAULT_SORT_BY } from '../utils/config';
 
 export const UrlContext = createContext();
 
 export default function UseUrlProvider({ children }) {
   const [language, setLanguage] = useState(DEFAULT_LANGUAGE);
+  const [sort, setSort] = useState(DEFAULT_SORT_TAG);
+
   const [page, setPage] = useState(1);
   const [sortBy, setSortBy] = useState(DEFAULT_SORT_BY);
   const url = request.searchIssues(language, page, sortBy);
--- src/utils/config.js
+++ src/utils/config.js
@@ -22,6 +22,8 @@ export const DEFAULT_SORT_BY = {
   order: 'desc',
 };
 export const DEFAULT_LANGUAGE = 'all';
+export const SORT_TAGS = ['comments', 'reactions', 'updated', 'best-match'];
+export const DEFAULT_SORT_TAG = 'best-match';
 export const ISSUE_PER_PAGE = 10;
 export const MAX_ISSUES_ALLOWED = 1000;
 export const TIMEOUT_SEC = 10;
