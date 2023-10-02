5137496ab23aeb4c78799c5d33bf53dcadb9ffc1
akartis-dev <sramamonjinirina@gmail.com>
2023-10-02 08:04:05 +0300

    fix avoid "react is undefined" in all test-case with typescript

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/__tests__/setup.js'],
