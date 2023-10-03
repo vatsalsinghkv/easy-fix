598e0b2a499522f5700ea91478e4ea938effa85a
Vatsal Singh <68834718+vatsalsinghkv@users.noreply.github.com>
2023-10-03 21:54:29 +0530

    fix: header size issue

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -11,20 +11,32 @@ const Header = () => {
   };
 
   return (
-    <header className={`flex justify-around py-1 border-b sm:py-5 ${darkMode ? 'border-dark-3' : 'border-light-3'}`}>
-      <Logo title='Easy Fix' className='container' />
-      <div className='self-center'>
-        <a href="https://github.com/vatsalsinghkv/easy-fix" target="_blank" rel="noopener noreferrer">
-          <button className={`inline-block p-3 py-1.5 font-mono text-xs capitalize transition-all border rounded cursor-pointer hover:text-accent hover:border-accent focus:text-accent focus:border-accent ${darkMode ? 'border-slate-400' : 'border-white'}`}>
-            View Github
+    <header className={`py-1 border-b sm:py-5 ${darkMode ? 'border-dark-3' : 'border-light-3'}`}>
+         <div className='container flex items-center justify-between'>
+        <Logo title='Easy Fix' />
+        <div>
+          <a
+            href='https://github.com/vatsalsinghkv/easy-fix'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <button
+              className={`inline-block p-3 py-1.5 font-mono text-xs capitalize transition-all border rounded cursor-pointer hover:text-accent hover:border-accent focus:text-accent focus:border-accent ${
+                darkMode ? 'border-slate-400' : 'border-white'
+              }`}
+            >
+              View Github
+            </button>
+          </a>
+          <button
+            className={`ml-[10px] inline-block p-3 py-1.5 font-mono text-xs capitalize transition-all border rounded cursor-pointer ${
+              darkMode ? 'border-slate-400' : 'border-white'
+            } `}
+            onClick={toggleDarkMode}
+          >
+            {darkMode ? 'Light Mode' : 'Dark Mode'}
           </button>
-        </a>
-        <button
-          className={`ml-[10px] inline-block p-3 py-1.5 font-mono text-xs capitalize transition-all border rounded cursor-pointer ${darkMode ? 'border-slate-400' : 'border-white'} `}
-          onClick={toggleDarkMode}
-        >
-          {darkMode ? 'Light Mode' : 'Dark Mode'}
-        </button>
+        </div>
       </div>
     </header>
   );
