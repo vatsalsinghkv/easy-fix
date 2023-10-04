d1b3280762a7835654224cb3807dd8af3dfffb97
Premsai Varma <premsaivarmacoding@gmail.com>
2023-10-04 23:39:50 +0530

    updated the Header

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
   };
 
   return (
+<<<<<<< HEAD
     <header className={`py-1 border-b sm:py-5 ${darkMode ? 'border-dark-3' : 'border-light-3'}`}>
          <div className='container flex items-center justify-between'>
         <Logo title='Easy Fix' />
@@ -37,6 +38,23 @@ const Header = () => {
             {darkMode ? 'Light Mode' : 'Dark Mode'}
           </button>
         </div>
+=======
+    <header className={`flex justify-around py-1 border-b sm:py-5 ${darkMode ? 'border-dark-3' : 'border-light-3'}`}>
+      <Logo title='Easy Fix' className='container self-center my-auto' />
+      <div className='self-center flex'>
+        <a href="https://github.com/vatsalsinghkv/easy-fix" target="_blank" rel="noopener noreferrer">
+          <button className={`inline-block p-3 py-1.5 font-mono text-xs capitalize transition-all border-none lg:border rounded  cursor-pointer hover:text-accent hover:border-accent focus:text-accent focus:border-accent ${darkMode ? 'border-slate-400' : 'border-white'} lg:flex`}>
+            <i className="fab fa-github mr-2 fa-2x"></i>
+            <span className='my-auto text-lg hidden lg:block'>Github</span>
+          </button>
+        </a>
+        <button
+          className={`ml-10 inline-block p-3 py-1.5 font-mono text-xs capitalize transition-all rounded cursor-pointer ${darkMode ? '' : ''}`}
+          onClick={toggleDarkMode}
+        >
+          {darkMode ? <i className="fas fa-sun text-black fa-2x"></i> : <i className="fas fa-moon text-black fa-2x"></i>}
+        </button>
+>>>>>>> d50530e (updated the github logo code)
       </div>
     </header>
   );
