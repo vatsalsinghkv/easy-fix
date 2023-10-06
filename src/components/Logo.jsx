8ad7e31fd62103d628789a1ac94bb414d8afab8b
premsaivarmachekuri <premsaivarmacoding@gmail.com>
2023-10-06 21:49:15 +0530

    made the github button responsive

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -28,7 +28,13 @@ const Logo = ({ title, classNme }) => {
           <span className='font-mono text-accent'>{rest.join(' ')}</span>
         </a>
       </h1>
-
+      <div className='self-center flex'>
+      <a href="https://github.com/vatsalsinghkv/easy-fix" target="_blank" rel="noopener noreferrer">
+      <button  className='flex self-center mr-[10px] inline-block p-3 py-1.5 font-mono text-xs capitalize transition-all border rounded cursor-pointer hover:text-accent hover:border-accent focus:text-accent focus:border-accent border-slate-400  2eer-checked:text-accent peer-checked:text-accent peer-checked:border-accent peer-checked:bg-accent-light sm:border-0 md:border-2'>
+      <i className="fab fa-github mr-2 fa-2x"></i>
+      <span className='my-auto text-lg hidden md:block'>Github</span>
+    </button>
+    </a>
       <button onClick={handleTheme} className=' w-[10vh] max-sm:translate-x-6'>
         {theme === 'dark' ? (
           <HiSun className='text-3xl ' />
@@ -36,6 +42,7 @@ const Logo = ({ title, classNme }) => {
           <HiMoon className='text-3xl ' />
         )}
       </button>
+      </div>
     </div>
   );
 };
