0c8389fa8243e9f0dbedbb60b0a2f7bc19561308
vatsal <vatsalsinghkv@gmail.com>
2023-10-06 22:09:57 +0530

    fix: dark mode (hook)

---
+++ src/lib/hooks/use-theme.tsx
@@ -0,0 +1,70 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+const initialState = {
+  isDarkMode: false,
+  toggle: () => {
+    return;
+  },
+  enableDarkMode: (_: boolean) => {
+    return;
+  },
+  disableDarkMode: (_: boolean) => {
+    return;
+  },
+};
+
+const ThemeContext = createContext(initialState);
+
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(
+    JSON.parse(localStorage.getItem('darkMode') || 'true') ? true : false
+  );
+
+  const toggle = useCallback(() => {
+    setIsDarkMode((prev) => !prev);
+  }, []);
+
+  const enableDarkMode = useCallback(() => {
+    setIsDarkMode(true);
+  }, []);
+
+  const disableDarkMode = useCallback(() => {
+    setIsDarkMode(false);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        isDarkMode,
+        toggle,
+        enableDarkMode,
+        disableDarkMode,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
