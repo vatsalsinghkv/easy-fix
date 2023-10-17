282277250747900fabc8932279668b8ef64c71b3
Bence Fekete <fekete965@gmail.com>
2023-10-17 23:04:05 +0100

    Add useAsync hook

---
+++ src/lib/hooks/useAsync/index.tsx
@@ -0,0 +1,100 @@
+import { useCallback, useEffect, useMemo, useReducer, useRef } from 'react';
+
+import getReducer, { State, getDefaultState } from './reducer';
+
+type Options = {
+  autoFetch: boolean;
+  onError: () => void;
+  onSuccess: () => void;
+};
+
+type RequestReturnValue<TData extends any> = {
+  run: () => void;
+} & State<TData>;
+
+export function useAsync<TData extends any = unknown>(
+  promiseFn: (signal?: AbortSignal) => Promise<TData>,
+  options?: Partial<Options>
+): RequestReturnValue<TData> {
+  const isMountedRef = useRef<boolean>(false);
+  const optionsRef = useRef<Partial<Options> | undefined>(options);
+  const promiseFnRef =
+    useRef<(signal?: AbortSignal) => Promise<TData>>(promiseFn);
+  const abortControllerRef = useRef<AbortController>(new AbortController());
+  const [{ data, error, isPending, isRejected, isResolved, status }, dispatch] =
+    useReducer(getReducer<TData>(), getDefaultState());
+
+  promiseFnRef.current = promiseFn;
+  optionsRef.current = options;
+
+  const run = useCallback(() => {
+    const abortController = abortControllerRef.current;
+    if (!abortController.signal.aborted) {
+      abortController.abort();
+    }
+    abortControllerRef.current = new AbortController();
+
+    dispatch({ type: 'load' });
+
+    const options = optionsRef.current;
+
+    promiseFnRef
+      .current(abortControllerRef.current.signal)
+      .then((payload) => {
+        dispatch({ type: 'resolved', payload });
+
+        options?.onSuccess && options?.onSuccess();
+
+        return payload;
+      })
+      .catch((error) => {
+        if (error instanceof Error) {
+          dispatch({ type: 'rejected', payload: error });
+
+          options?.onError && options?.onError();
+
+          return Promise.reject(error);
+        }
+
+        const customError = new Error('Something went wrong');
+        dispatch({ type: 'rejected', payload: customError });
+
+        return Promise.reject(customError);
+      });
+  }, []);
+
+  useEffect(() => {
+    isMountedRef.current = true;
+
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (options?.autoFetch && isMountedRef.current) {
+      run();
+    }
+
+    return () => {
+      abortControllerRef.current.abort();
+    };
+  }, [options?.autoFetch, run]);
+
+  const value = useMemo(
+    () => ({
+      data,
+      error,
+      isPending,
+      isRejected,
+      isResolved,
+      run,
+      status,
+    }),
+    [data, error, isPending, isRejected, isResolved, run, status]
+  );
+
+  return value as RequestReturnValue<TData>;
+}
+
+export default useAsync;
---
+++ src/lib/hooks/useAsync/reducer.ts
@@ -0,0 +1,108 @@
+import { Reducer } from 'react';
+
+interface IdleState {
+  data: null;
+  error: null;
+  isPending: false;
+  isRejected: false;
+  isResolved: false;
+  status: 'idle';
+}
+
+interface PendingState {
+  data: null;
+  error: null;
+  isPending: true;
+  isRejected: false;
+  isResolved: false;
+  status: 'pending';
+}
+
+interface ResolvedState<TData extends any> {
+  data: TData;
+  error: null;
+  isPending: false;
+  isRejected: false;
+  isResolved: true;
+  status: 'resolved';
+}
+
+interface RejectedState {
+  data: null;
+  error: Error;
+  isPending: false;
+  isRejected: true;
+  isResolved: false;
+  status: 'rejected';
+}
+
+export type State<TData extends any> =
+  | IdleState
+  | PendingState
+  | RejectedState
+  | ResolvedState<TData>;
+
+type LoadAction = {
+  type: 'load';
+};
+
+type ResolvedAction<TData extends any> = {
+  type: 'resolved';
+  payload: TData;
+};
+
+type RejectedAction = {
+  type: 'rejected';
+  payload: Error;
+};
+
+type Action<TData> = LoadAction | RejectedAction | ResolvedAction<TData>;
+
+export function getDefaultState(): IdleState {
+  return {
+    data: null,
+    error: null,
+    isPending: false,
+    isRejected: false,
+    isResolved: false,
+    status: 'idle',
+  };
+}
+
+function getReducer<TData extends any>(): Reducer<State<TData>, Action<TData>> {
+  return function (state, action) {
+    switch (action.type) {
+      case 'load':
+        return {
+          data: null,
+          error: null,
+          isPending: true,
+          isRejected: false,
+          isResolved: false,
+          status: 'pending',
+        };
+      case 'rejected':
+        return {
+          data: null,
+          error: action.payload,
+          isPending: false,
+          isRejected: true,
+          isResolved: false,
+          status: 'rejected',
+        };
+      case 'resolved':
+        return {
+          data: action.payload,
+          error: null,
+          isPending: false,
+          isRejected: false,
+          isResolved: true,
+          status: 'resolved',
+        };
+      default:
+        return state;
+    }
+  };
+}
+
+export default getReducer;
