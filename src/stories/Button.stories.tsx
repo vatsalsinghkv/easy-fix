6b96c96544c1053808502713a1e13c6ca5608b3d
Bence Fekete <fekete965@gmail.com>
2023-11-01 22:37:29 +0000

    Add Button component with stories as an example

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@/components/Button';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Default Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    children: 'Small Button',
+    size: 'sm',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    children: 'Medium Button',
+    size: 'md',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    children: 'Large Button',
+    size: 'lg',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: 'Disabled Button',
+    disabled: true,
+  },
+};
+
+export const Selected: Story = {
+  args: {
+    children: 'Selected Button',
+    selected: true,
+  },
+};
