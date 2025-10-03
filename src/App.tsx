0be9b48e1a7939378d9afd662038a2e940f0253d
Vatsal Singh <68834718+vatsalsinghkv@users.noreply.github.com>
2025-10-03 13:42:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
       <main className='container'>
         <div className='grid grid-cols-1 gap-5 md:grid-cols-[1.25fr_3fr]'>
-          <aside className='pt-5 pb-4 space-y-5 md:pr-5 md:border-r border-dark-3'>
+          <aside className='py-5 space-y-5 md:pr-5 md:border-r border-dark-3'>
             <MiniContainer title='about'>
               The perfect place to find{' '}
               <span className='text-accent'>easy-to-fix</span> issues. Goal is
